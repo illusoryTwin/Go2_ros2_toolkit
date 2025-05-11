328dd1f7eeffb3a06fc8c057d516f86a5f730bf0
ekaterina <mozhegova04@gmail.com>
2025-05-11 09:30:57 +0300

    updated launch scripts

---
+++ main_launch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e  # Exit immediately if a command fails
+
+# Open first terminal and run ROS 2 workspace commands
+gnome-terminal -- bash -c "
+source prelaunch.sh;
+./bringup_launch.sh;
+exec bash
+"
+
+# Open second terminal and run the virtual environment and Python script
+gnome-terminal -- bash -c "
+source venv/bin/activate;
+cd external/unitree_mujoco/simulate_python;
+python3 ./unitree_mujoco.py;
+exec bash
+"
+
+# Wait for both processes to finish (it will only wait for the second process, ROS 2 is running in its terminal)
+wait
+
